c2b5320eaeb594a0004715239a01a171eea0a7e5
chaitra-techmust <chaitrav1512@gmail.com>
2022-07-30 14:51:33 +0530

    paragraph tag

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ function App()
                        Even if I wanted to type one thousand characters, I have to go to bed now anyway, so I simply don't have the time. 
                       I mean, I could just type a bunch of random letters or hold down one key, but that would be no fun at all.
                       </p>
+                     
+                  
+                      {/* <textarea placeholder="500 character" className='para'>
+                      
+                      </textarea> */}
+
                     </div>
 
                     <button> SUBMIT </button>
